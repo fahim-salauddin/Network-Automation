8b28ac7a1c35334a41f50b322b95e93dfc3a0716
Fahim Salauddin <fahim.prince@gmail.com>
2025-12-05 21:36:39 +1100

    Add basic Netmiko script for Cisco device

---
+++ Netmiko_4.py
@@ -0,0 +1,20 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+cisco1 = {
+    "device_type": "cisco_ios",
+    "host": "ESW2",
+    "username": "cisco",
+    "password": "cisco",
+}
+
+# Show command that we execute.
+command = "show ip int brief"
+
+with ConnectHandler(**cisco1) as net_connect:
+    output = net_connect.send_command(command)
+
+# Automatically cleans-up the output so that only the show output is returned
+print()
+print(output)
+print()

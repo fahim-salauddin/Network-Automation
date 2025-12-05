02e389d49d43899280a21eb90c0a03f4565795a3
Fahim Salauddin <fahim.prince@gmail.com>
2025-12-05 20:55:17 +1100

    Add script to connect and retrieve device hostnames
    This script connects to multiple Cisco IOS devices, retrieves their hostname, and prints the details in JSON format.

---
+++ Netmiko_2.py
@@ -0,0 +1,35 @@
+from netmiko import ConnectHandler
+import getpass
+import json
+
+passwd = getpass.getpass('Please enter the password: ')
+
+# List of device IPs
+ip_list = ["192.168.1.51", "192.168.1.52"]
+
+# Create a list of dictionaries for each device
+device_list = []
+
+# Populate the device list with device details
+for ip in ip_list:
+        device = {
+                "device_type": "cisco_ios",
+                "host": ip,
+                "username": "cisco",
+                "password": passwd, # Log in password from getpass
+                "secret": passwd # Enable password from getpass
+        }
+        device_list.append(device)
+# Print human-readable device details using JSON formatting
+json_formatted = json.dumps(device_list, indent=4)
+print(json_formatted)
+
+# Iterate over each device and connect to it
+for each_device in device_list:
+        connection = ConnectHandler(**each_device)
+        connection.enable()
+        print(f'Connecting to {each_device["host"]}')
+        output = connection.send_command('show run | include hostname')
+        print(output)
+        print(f'Closing Connection on {each_device["host"]}')
+        connection.disconnect()
